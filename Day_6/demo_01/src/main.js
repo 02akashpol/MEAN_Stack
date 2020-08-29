711670f1df9f8a8e39919eb977678bfafecc6760
Akash_Pol <65165883+02akashpol@users.noreply.github.com>
2020-08-29 09:25:05 +0530

    Add files via upload

---
+++ Day_6/demo_01/src/main.js
@@ -0,0 +1,22 @@
+// imorting object files and assigning to the var
+const PI = require("./modules/module1");
+
+const module2 = require("./modules/module2");
+
+const module3 = require("./modules/module3");
+
+const module4 = require("./modules/module4");
+
+console.log("Executing main file funs.");
+
+// printing values from module1
+console.log("Values from module1 : ", PI);
+
+// printing values from module2
+console.log("Values from module2 : ", module2);
+
+// printing values from module3
+console.log("Values from module3 : ", module3);
+
+// printing values from module4
+console.log("Values from module4 : ", module4);
