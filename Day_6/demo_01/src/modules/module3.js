db3866e93080efd94197e76f42a825b37aa71fb3
Akash_Pol <65165883+02akashpol@users.noreply.github.com>
2020-08-29 09:26:50 +0530

    Add files via upload

---
+++ Day_6/demo_01/src/modules/module3.js
@@ -0,0 +1,15 @@
+const num1 = 20;
+
+const num2 = 30.50;
+
+const isTrue = false;
+
+const str = "Welcome to NodeJS.";
+
+module.exports = {
+
+  num1 : num1,
+  num2 : num2,
+  booleanValue : isTrue,
+  str : str
+};
---
+++ Day_6/demo_01/src/modules/module4.js
@@ -0,0 +1,15 @@
+const num1 = 20;
+
+const num2 = 30.50;
+
+const isTrue = false;
+
+const str = "Welcome to NodeJS.";
+
+module.exports = {
+
+  num1,
+  num2,
+  isTrue,
+  str
+};
